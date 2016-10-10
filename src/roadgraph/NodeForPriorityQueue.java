a2721d7b40239794cb23606d56ad76719f2b2f34
butchsergiy <1144777@gmail.com>
2016-10-10 10:27:02 +0300

    little refactoring

--- src/roadgraph/NodeForPriorityQueue.java
+++ src/roadgraph/NodeForPriorityQueue.java
@@ -10,9 +10,17 @@ class NodeForPriorityQueue {
     private GeographicPoint nodeGeoPoint;
     private double distanceToStartNode;
 
+    private double distanceToEndNode;
+
     public NodeForPriorityQueue(GeographicPoint nodeGeoPoint, double distanceToStartNode) {
         this.nodeGeoPoint = nodeGeoPoint;
         this.distanceToStartNode = distanceToStartNode;
+    }
+
+    public NodeForPriorityQueue(GeographicPoint nodeGeoPoint, double distanceToStartNode, double distanceToEndNode) {
+        this.nodeGeoPoint = nodeGeoPoint;
+        this.distanceToStartNode = distanceToStartNode;
+        this.distanceToEndNode = distanceToEndNode;
     }
 
     public GeographicPoint getNodeGeoPoint() {
@@ -29,5 +37,28 @@ class NodeForPriorityQueue {
 
     public void setDistanceToStartNode(double distanceToStartNode) {
         this.distanceToStartNode = distanceToStartNode;
+    }
+
+    public double getDistanceToEndNode() {
+        return distanceToEndNode;
+    }
+
+    public void setDistanceToEndNode(double distanceToEndNode) {
+        this.distanceToEndNode = distanceToEndNode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NodeForPriorityQueue that = (NodeForPriorityQueue) o;
+
+        return nodeGeoPoint.equals(that.nodeGeoPoint);
+    }
+
+    @Override
+    public int hashCode() {
+        return nodeGeoPoint.hashCode();
     }
 }
